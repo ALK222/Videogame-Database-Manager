d2392ff0c9227606563582ce8a59c3a3ffb4cdf6
A.L.K <alejandrobarrachina.02@gmail.com>
2020-09-27 13:09:13 +0200

    database connection goes brr

--- Scripts/databaseManager/datab.rb
+++ Scripts/databaseManager/datab.rb
@@ -2,21 +2,25 @@
 
 require 'mysql2'
 
-class dbManager
+class DbManager
     def initialize(*args)
         @client = Mysql2::Client.new(
             :host => args[0],
             :username => args[1],
             :password => args[2],
-            :database => args[3],
-            :encoding => args[4],
+            :database => args[3]
         )
-        @list = client.query("SELECT * FROM games JOIN game_modes")
+        @list = @client.query("SELECT * FROM `games`")
     end
 
-    def printList
-        @list.each{ |game|
-            puts game
-        }
+    def getList
+        return @list
+    end
+
+    def updatable(game)
+        if(game["release_date"] == nil)
+            return true
+        end
+        return false
     end
 end
--- Scripts/main.rb
+++ Scripts/main.rb
@@ -2,7 +2,18 @@
 
 require "./databaseManager/datab.rb"
 require "./gameManager/Games.rb"
-require "dotenv"
+require 'dotenv'
 
-Dotenv.load('.env')
-db = dbManager.new()
+Dotenv.load()
+db = DbManager.new(
+    ENV["DB_HOST"],
+    ENV["DB_USER"],
+    ENV["DB_PASS"],
+    ENV["DB_DATABASE"],
+)
+
+db.getList.each{ |game|
+    if(db.updatable(game))
+        
+    end
+}
